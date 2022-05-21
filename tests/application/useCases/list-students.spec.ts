3d8091934301a9e4646db4b3b0101cb0eb956098
KPMGE <jesuskevin254@gmail.com>
2022-05-21 20:09:30 -0300

    :white_check_mark: ensure ListStudentsService calls repository only once

---
+++ tests/application/useCases/list-students.spec.ts
@@ -0,0 +1,52 @@
+type CourseWork = {
+  title: string
+  description: string
+};
+
+type Submission = {
+  late: boolean
+  courseWork: CourseWork
+};
+
+type Student = {
+  id: string
+  name: string
+  email: string
+  submissions: Submission[]
+};
+
+interface ListStudentsRepository {
+  list(): Promise<Student[]>
+}
+
+interface ListStudentsUseCase {
+  list(): Promise<Student[]>
+}
+
+class ListStudentsRepositoryMock implements ListStudentsRepository {
+  callsCount = 0
+  async list(): Promise<Student[]> {
+    this.callsCount++
+    return null
+  }
+}
+
+class ListStudentsService implements ListStudentsUseCase {
+  constructor(private readonly listStudentsRepo: ListStudentsRepository) { }
+
+  async list(): Promise<Student[]> {
+    this.listStudentsRepo.list()
+    return null
+  }
+}
+
+describe('list students', () => {
+  it('should call repository only once', () => {
+    const repo = new ListStudentsRepositoryMock()
+    const sut = new ListStudentsService(repo)
+
+    sut.list()
+
+    expect(repo.callsCount).toBe(1)
+  })
+})
