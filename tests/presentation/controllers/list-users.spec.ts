d0b62b8e6acc0134a057194d6f929527c21ffd31
KPMGE <jesuskevin254@gmail.com>
2022-05-21 22:32:23 -0300

    :white_check_mark: ensure ListStudentsController returns serverError if service throws

--- tests/presentation/controllers/list-users.spec.ts
+++ tests/presentation/controllers/list-users.spec.ts
@@ -1,11 +1,14 @@
 import { Student } from "../../../src/domain/entities"
 import { ListStudentsUseCase } from "../../../src/domain/protocols"
+import { makeStudent } from "../../domain/mocks"
 
 class ListStudentsServiceMock implements ListStudentsUseCase {
   callsCount = 0
+  students = [makeStudent()]
+
   async list(): Promise<Student[]> {
     this.callsCount++
-    return null
+    return this.students
   }
 }
 
@@ -18,12 +21,21 @@ interface Controller {
   handle(): Promise<HttpResponse>
 }
 
+const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error
+})
+
+
 class ListStudentsController implements Controller {
   constructor(private readonly listStudentsService: ListStudentsUseCase) { }
 
   async handle(): Promise<HttpResponse> {
-    this.listStudentsService.list()
-    return null
+    try {
+      await this.listStudentsService.list()
+    } catch (err) {
+      return serverError(err)
+    }
   }
 }
 
@@ -48,5 +60,15 @@ describe('list students', () => {
     sut.handle()
 
     expect(serviceMock.callsCount).toBe(1)
+  })
+
+  it('should return serverError if service throws', async () => {
+    const { sut, serviceMock } = makeSut()
+    serviceMock.list = () => { throw new Error('service error') }
+
+    const httpResponse = await sut.handle()
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new Error('service error'))
   })
 })
